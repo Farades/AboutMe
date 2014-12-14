2b45459a9110f6e895a8bf075c2b46902a22c63a
farades <creg2007@gmail.com>
2014-12-14 22:38:22 +0400

    add copy to clipBoard

--- app/src/main/java/ru/mtplab/aboutme/MainActivity.java
+++ app/src/main/java/ru/mtplab/aboutme/MainActivity.java
@@ -1,5 +1,9 @@
 package ru.mtplab.aboutme;
 
+import android.app.AlertDialog;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -70,15 +74,42 @@ public class MainActivity extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         if (id == R.id.menu_copy) {
-            nameText.setText("Тест");
+            String copyText = "ФИО: " + getString(R.string.last_name) + " " + getString(R.string.first_name) + "\n"
+                    + "Email: " + getString(R.string.email) + "\n"
+                    + "Телефон: " + getString(R.string.phone);
+            saveToClipboard(copyText);
+            showDialog("Копирование", "ФИО, email, телефон скопированы в буфер обмена.");
+        } else if (id == R.id.menu_copy_name) {
+            String copyText = "ФИО: " + getString(R.string.last_name) + " " + getString(R.string.first_name);
+            saveToClipboard(copyText);
+            showDialog("Копирование ФИО", "ФИО скопированы в буфер обмена.");
+        } else if (id == R.id.menu_copy_email) {
+            String copyText = "Email: " + getString(R.string.email);
+            saveToClipboard(copyText);
+            showDialog("Копирование email", "Email скопирован в буфер обмена.");
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void showDialog(String title, String message) {
+        AlertDialog.Builder dlgBuilder = new AlertDialog.Builder(this);
+        dlgBuilder.setTitle(title);
+        dlgBuilder.setMessage(message);
+        dlgBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        AlertDialog Dialog = dlgBuilder.create();
+        Dialog.show();
+    }
+
+    private void saveToClipboard(String text) {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("AboutMe", text);
+        clipboard.setPrimaryClip(clip);
     }
 }
